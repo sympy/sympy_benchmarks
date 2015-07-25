95c91718d1b3899e4773789c854ef29466bb62a6
Sudhanshu Mishra <mrsud94@gmail.com>
2015-07-25 19:44:17 +0530

    Split both algorithm benchmarks in two parts

--- benchmarks/logic.py
+++ benchmarks/logic.py
@@ -12,13 +12,18 @@ class LogicSuite:
     Benchmark suite for sympy.logic
     """
     def setup(self):
-        self.INPUT = [5 * i for i in range(2, 16)]
-        self.ALGORITHMS = ['dpll', 'dpll2']
+        self.INPUT = [5*i for i in range(2, 16)]
 
-    def time_logic():
+    def time_dpll(self):
         for test in self.INPUT:
-            for alg in self.ALGORITHMS:
-                file_name = os.path.join(input_path, 'logic-inputs', '%s.cnf' % test)
-                theory = load_file(file_name)
-                if not satisfiable(theory, algorithm=alg):
-                    raise ValueError("Function returned false")
+            file_name = os.path.join(input_path, 'logic-inputs', '%s.cnf' % test)
+            theory = load_file(file_name)
+            if not satisfiable(theory, algorithm='dpll'):
+                raise ValueError("Function returned false")
+
+    def time_dpll2(self):
+        for test in self.INPUT:
+            file_name = os.path.join(input_path, 'logic-inputs', '%s.cnf' % test)
+            theory = load_file(file_name)
+            if not satisfiable(theory, algorithm='dpll2'):
+                raise ValueError("Function returned false")
