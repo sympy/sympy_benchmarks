0561c5f33c61d050b4c03eebb16a4cfc663ced6f
Sudhanshu Mishra <mrsud94@gmail.com>
2015-07-26 15:07:29 +0530

    Add load_file benchmark and move file loads to setup

--- benchmarks/logic.py
+++ benchmarks/logic.py
@@ -13,17 +13,22 @@ class LogicSuite:
     """
     def setup(self):
         self.INPUT = [5*i for i in range(2, 16)]
-
-    def time_dpll(self):
+        self.theories = []
         for test in self.INPUT:
             file_name = os.path.join(input_path, 'logic-inputs', '%s.cnf' % test)
             theory = load_file(file_name)
+            self.theories.append(theory)
+
+    def time_load_file(self):
+        file_name = os.path.join(input_path, 'logic-inputs', '10.cnf')
+        load_file(file_name)
+
+    def time_dpll(self):
+        for theory in self.theories:
             if not satisfiable(theory, algorithm='dpll'):
                 raise ValueError("Function returned false")
 
     def time_dpll2(self):
-        for test in self.INPUT:
-            file_name = os.path.join(input_path, 'logic-inputs', '%s.cnf' % test)
-            theory = load_file(file_name)
+        for theory in self.theories:
             if not satisfiable(theory, algorithm='dpll2'):
                 raise ValueError("Function returned false")
