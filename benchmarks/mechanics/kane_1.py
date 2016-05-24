bd93ccead23ff512d10d14877be4dfa6e73d7506
James Brandon Milam <jmilam343@gmail.com>
2016-05-24 16:09:53 -0400

    Added a test for KanesMethod and LagrangesMethod
    Created a benchmark test for KanesMethod and Lagranges method in a new
    subfolder mechanics. The tests run the imports and most of the code in a
    setup statement and only time the run of EOMMethod() and
    form_equations().

---
+++ benchmarks/mechanics/kane_1.py
@@ -0,0 +1,23 @@
+class Suite:
+    def setup(self):
+        # This is taken from the example in KanesMethod docstring
+        from sympy import symbols
+        import sympy.physics.mechanics as me
+
+        self.me = me
+        self.q, self.u = me.dynamicsymbols('q u')
+        qd, ud = me.dynamicsymbols('q u', 1)
+        m, c, k = symbols('m c k')
+        self.N = me.ReferenceFrame('N')
+        P = me.Point('P')
+        P.set_vel(self.N, self.u * self.N.x)
+
+        self.kd = [qd - self.u]
+        self.FL = [(P, (-k * self.q - c * self.u) * self.N.x)]
+        pa = me.Particle('pa', P, m)
+        self.BL = [pa]
+
+    def time_kanesmethod(self):
+        KM = self.me.KanesMethod(self.N, q_ind=[self.q], u_ind=[self.u],
+                                 kd_eqs=self.kd)
+        (fr, frstar) = KM.kanes_kquations(self.BL, self.FL)
---
+++ benchmarks/mechanics/lagrange_1.py
@@ -0,0 +1,41 @@
+class Suite:
+    def setup(self):
+        from sympy import symbols
+        import sympy.physics.mechanics as me
+        self.me = me
+
+        # System state variables
+        self.theta = me.dynamicsymbols('theta')
+        thetad = me.dynamicsymbols('theta', 1)
+
+        # Other system variables
+        m, l, g = symbols('m l g')
+
+        # Set up the reference frames
+        # Reference frame A set up in the plane perpendicular to the page
+        # containing segment OP
+        self.N = me.ReferenceFrame('N')
+        A = self.N.orientnew('A', 'Axis', [self.theta, self.N.z])
+
+        # Set up the points and particles
+        O = me.Point('O')
+        P = O.locatenew('P', l * A.x)
+
+        Pa = me.Particle('Pa', P, m)
+
+        # Set up velocities
+        A.set_ang_vel(self.N, thetad * self.N.z)
+        O.set_vel(self.N, 0)
+        P.v2pt_theory(O, self.N, A)
+
+        # Set up the lagrangian
+        self.L = me.Lagrangian(self.N, Pa)
+
+        # Create the list of forces acting on the system
+        self.fl = [(P, g * m * self.N.x)]
+
+    def lmethod(self):
+        # Create the equations of motion using lagranges method
+        l = self.me.LagrangesMethod(self.L, [self.theta], forcelist=self.fl,
+                                    frame=self.N)
+        l.form_lagranges_equations()
