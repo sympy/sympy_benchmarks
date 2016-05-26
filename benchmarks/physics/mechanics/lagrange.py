4dd9f8d033b6cb909ddbb593628d0a1f3affa4e1
James Brandon Milam <jmilam343@gmail.com>
2016-05-26 14:13:29 -0400

    Removed the pendulum lagranges method test
    The pendulum Lagranges Method test did not add additional test coverage
    when being included with the mass spring damper test and so it has been
    removed.

--- benchmarks/physics/mechanics/lagrange.py
+++ benchmarks/physics/mechanics/lagrange.py
@@ -2,47 +2,6 @@ from sympy import symbols
 import sympy.physics.mechanics as me
 
 
-class LagrangesMethodPendulum:
-    def setup(self):
-        # System state variables
-        theta = me.dynamicsymbols('theta')
-        thetad = me.dynamicsymbols('theta', 1)
-
-        # Other system variables
-        m, l, g = symbols('m l g')
-
-        # Set up the reference frames
-        # Reference frame A set up in the plane perpendicular to the page
-        # containing segment OP
-        N = me.ReferenceFrame('N')
-        A = N.orientnew('A', 'Axis', [theta, N.z])
-
-        # Set up the points and particles
-        O = me.Point('O')
-        P = O.locatenew('P', l * A.x)
-
-        Pa = me.Particle('Pa', P, m)
-
-        # Set up velocities
-        A.set_ang_vel(N, thetad * N.z)
-        O.set_vel(N, 0)
-        P.v2pt_theory(O, N, A)
-
-        # Set up the lagrangian
-        L = me.Lagrangian(N, Pa)
-
-        # Create the list of forces acting on the system
-        fl = [(P, g * m * N.x)]
-
-        # Create an instance of LagrangesMethod
-        self.l = me.LagrangesMethod(L, [theta], forcelist=fl,
-                                    frame=N)
-
-    def time_lagrangesmethod_pendulum(self):
-        # Create the equations of motion using lagranges method
-        self.l.form_lagranges_equations()
-
-
 class LagrangesMethodMassSpringDamper:
     def setup(self):
         # System state variables
