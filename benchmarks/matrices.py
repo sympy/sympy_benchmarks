c2442abd304592810a28d7d15005fe746678d4e5
Aaron Meurer <asmeurer@gmail.com>
2019-02-24 18:20:31 -0700

    Fix matrices benchmarks

--- benchmarks/matrices.py
+++ benchmarks/matrices.py
@@ -7,20 +7,20 @@ B = MatrixSymbol("B", n, n)
 args = (A, B, A*B, B*A, A**2, B**2, A**-1, B**-1, A*B**-1, B*A**-1, A**-1*B, B*A**-1, A**-2, B**-2, A**-1*B**-1, B**-1*A**-1, A, B, A*B, B*A, A**2, B**2, A**-1, B**-1, A*B**-1, B*A**-1, A**-1*B, B*A**-1, A**-2, B**-2, A**-1*B**-1, B**-1*A**-1)
 
 class TimeMatrixExpression:
-    def time_Add():
+    def time_Add(self):
         Add(*args)
 
-    def time_MatAdd():
+    def time_MatAdd(self):
         MatAdd(*args)
 
-    def time_MatAdd_doit():
+    def time_MatAdd_doit(self):
         MatAdd(*args).doit()
 
-    def time_Mul():
+    def time_Mul(self):
         Mul(*args)
 
-    def time_MatMul():
+    def time_MatMul(self):
         MatMul(*args)
 
-    def time_MatMul_doit():
+    def time_MatMul_doit(self):
         MatMul(*args).doit()
