f799347634974717b6e04e20a5bca67d421f2430
Björn Dahlgren <bjodah@gmail.com>
2019-04-22 11:58:49 +0200

    Use sympy-1.1.1 instead of sympy-1.1

--- .ci/run_ci.sh
+++ .ci/run_ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone --bare git://github.com/sympy/sympy sympy  # asv uses --bare
-for tag in $(cd sympy; git tag --contains sympy-1.0 | grep -v rc) master; do  # loops over releases since 1.0 (and master)
+for tag in sympy-1.0 sympy-1.1.1 $(cd sympy; git tag --contains sympy-1.2 | grep -v rc) master; do  # loops over releases since 1.0 (and master)
     python3 -m virtualenv /tmp/venv-${tag}
     echo "Running the benchmark test suite for ${tag}..."
     bash -c "source /tmp/venv-${tag}/bin/activate; pip install pytest ${tag} && pytest -rs"
