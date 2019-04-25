904fd4681d88469458da9d186a348e1328c6294c
Björn Dahlgren <bjodah@gmail.com>
2019-04-25 22:14:02 +0200

    Travis fold

--- .ci/run_ci.sh
+++ .ci/run_ci.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -eu
+if [ "$TRAVIS" -eq "true" ]; then echo -en "travis_fold:start:test-${tag}\\r"; fi
 tag=$1
 python3 -m virtualenv /tmp/venv-${tag}
 echo "Running the benchmark test suite for ${tag}..."
 bash -c "source /tmp/venv-${tag}/bin/activate; pip install pytest ${tag//-/==} && pytest -rs"
 echo "Running benchmarks for ${tag}..."
 asv run --machine travis_dummy_machine --config asv.conf.venv.json --show-stderr "${tag}^!"
+if [ "$TRAVIS" -eq "true" ]; then echo -en "travis_fold:end:test-${tag}\\r"; fi
