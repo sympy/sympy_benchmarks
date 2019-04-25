99ac3eec1b606bc7ff32838339b5db168fd877d8
Björn Dahlgren <bjodah@gmail.com>
2019-04-25 21:32:31 +0200

    explicit sections for releases

--- .ci/run_ci.sh
+++ .ci/run_ci.sh
@@ -1,9 +1,7 @@
-#!/bin/bash -e
-git clone --bare git://github.com/sympy/sympy sympy  # asv uses --bare
-for tag in sympy-1.0 sympy-1.1.1 $(cd sympy; git tag --contains sympy-1.2 | grep -v rc) master; do  # loops over releases since 1.0 (and master)
-    python3 -m virtualenv /tmp/venv-${tag}
-    echo "Running the benchmark test suite for ${tag}..."
-    bash -c "source /tmp/venv-${tag}/bin/activate; pip install pytest ${tag//-/==} && pytest -rs"
-    echo "Running benchmarks for ${tag}..."
-    asv run --machine travis_dummy_machine --config asv.conf.venv.json --show-stderr "${tag}^!"
-done
+#!/bin/bash -eu
+tag=$1
+python3 -m virtualenv /tmp/venv-${tag}
+echo "Running the benchmark test suite for ${tag}..."
+bash -c "source /tmp/venv-${tag}/bin/activate; pip install pytest ${tag//-/==} && pytest -rs"
+echo "Running benchmarks for ${tag}..."
+asv run --machine travis_dummy_machine --config asv.conf.venv.json --show-stderr "${tag}^!"
