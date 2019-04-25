c626b1509555ee2fd08fe4c81f2bcee3ff181e7f
Björn Dahlgren <bjodah@gmail.com>
2019-04-25 22:35:13 +0200

    simplify script

--- .ci/run_ci.sh
+++ .ci/run_ci.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -eu
 tag=$1
-if [[ "$TRAVIS" == "true" ]]; then echo -en "travis_fold:start:test-${tag}\\r"; fi
+if [[ ${TRAVIS:-} ]]; then echo -en "travis_fold:start:test-${tag}\\r"; fi
 python3 -m virtualenv /tmp/venv-${tag}
-echo "Running the benchmark test suite for ${tag}..."
-bash -c "source /tmp/venv-${tag}/bin/activate; pip install pytest ${tag//-/==} && pytest -rs"
+bash -c "source /tmp/venv-${tag}/bin/activate; pip install pytest https://github.com/sympy/sympy/archive/${tag}.tar.gz && echo \"Running the benchmark test suite for ${tag}...\" && pytest -rs"
 echo "Running benchmarks for ${tag}..."
 asv run --machine travis_dummy_machine --config asv.conf.venv.json --show-stderr "${tag}^!"
-if [[ "$TRAVIS" == "true" ]]; then echo -en "travis_fold:end:test-${tag}\\r"; fi
+if [[ ${TRAVIS:-} ]]; then echo -en "travis_fold:end:test-${tag}\\r"; fi
