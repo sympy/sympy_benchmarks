4ec4abc07dc74cb3918946c32e23ce5b4df13044
Björn Dahlgren <bjodah@gmail.com>
2019-04-25 22:52:10 +0200

    Correctly reference asv 'machine'

--- .ci/run_ci.sh
+++ .ci/run_ci.sh
@@ -4,5 +4,5 @@ if [[ ${TRAVIS:-} ]]; then echo -en "travis_fold:start:test-${tag}\\r"; fi
 python3 -m virtualenv /tmp/venv-${tag}
 bash -c "source /tmp/venv-${tag}/bin/activate; pip install pytest https://github.com/sympy/sympy/archive/${tag}.tar.gz && echo \"Running the benchmark test suite for ${tag}...\" && pytest -rs"
 echo "Running benchmarks for ${tag}..."
-asv run --machine travis_dummy_machine --config asv.conf.venv.json --show-stderr "${tag}^!"
+asv run --machine travis --config asv.conf.venv.json --show-stderr "${tag}^!"
 if [[ ${TRAVIS:-} ]]; then echo -en "travis_fold:end:test-${tag}\\r"; fi
