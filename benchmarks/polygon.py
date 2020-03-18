34b1680e00d69a6257c3c1356b32643ec888e4cd
OmarWagih1 <o.wagih.ow@gmail.com>
2020-03-18 19:21:06 +0200

    Using sympy.version and PolygonAttributes

--- benchmarks/polygon.py
+++ benchmarks/polygon.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division
-
+from distutils.version import LooseVersion, StrictVersion
 from sympy import Rational
+import sympy
 from sympy.geometry import (Line, Point,
                             Polygon)
 from random import randint
@@ -11,36 +12,50 @@ listOfPolygons = [Polygon(Point(0+w,0+w),
 cutListOfPolygons = [Polygon((-1, -1), (1, Rational(5, 2)), (2, 1), (3, Rational(5, 2)), (4, 2), (5, 3), (-1, 3)) for w in range(10)]
 cutLines = [Line((0, 0), (Rational(9, 2), 3)) for w in range(10)]
 
-class PolygonCreate:	
-    def time_bench01(self):
+class PolygonAttributes:
+    def time_create(self):
         "Creating Polygon"
         Polygon(Point(0, 0), Point(3, -1),Point(6, 0), Point(4, 5))
 
-class PolygonArea:
-    def time_bench01(self):
+    def time_area(self):
         "Polygon.area for w in range(10)"
         [listOfPolygons[w].area for w in range(10)]
 
-class PolygonPerimeter:
-    def time_bench01(self):
+    def time_perimeter(self):
         "Polygon.perimeter for w in range(10)"
         [listOfPolygons[w].perimeter for w in range(10)]
 
-class PolygonSides:
-    def time_bench01(self):
+    def time_sides(self):
         "Polygon.sides for w in range(10)"
         [listOfPolygons[w].sides for w in range(10)]
 
-class PolygonCentroid:
-    def time_bench01(self):
+    def time_centroid(self):
         "Polygon.centroid for w in range(10)"
         [listOfPolygons[w].centroid for w in range(10)]
 
+class PolygonIsConvex:
+    def time_bench01(self):
+        "Polygon.is_convex() for w in range(10)"
+        [listOfPolygons[w].is_convex() for w in range(10)]
+
+class PolygonEnclosesPoint:
+    def time_bench01(self,Point2=Point(randint(0,10),randint(0,10))):
+        "Polygon.encloses_point for w in range(10)"
+        [listOfPolygons[w].encloses_point(Point2) for w in range(10)]
+
+class PolygonArbitraryPoint:
+    def time_bench01(self):
+        "Polygon.arbitrary_point for w in range(10)"
+        [listOfPolygons[w].arbitrary_point() for w in range(10)]
+
+class PolygonDistance:
+    def time_bench01(self):
+        "Polygon.distance() for w in range(10)"
+        [listOfPolygons[w].distance(Point(randint(0,10),randint(0,10))) for w in range(10)]
+
 class PolygonSecondMoment:
     def setup(self):
-        try:
-            listOfPolygons[0].second_moment_of_area()
-        except AttributeError:
+        if StrictVersion(sympy.__version__) < StrictVersion("1.3.0"):
             raise NotImplementedError
     def time_bench01(self):
         "Polygon.second_moment_of_area() for w in range(10)"
@@ -48,9 +63,7 @@ class PolygonSecondMoment:
 
 class PolygonFirstMoment:
     def setup(self):
-        try:
-            listOfPolygons[0].first_moment_of_area()
-        except AttributeError:
+        if StrictVersion(sympy.__version__) < StrictVersion("1.5.0"):
             raise NotImplementedError
     def time_bench01(self):
         "Polygon.first_moment_of_area for w in range(10)"
@@ -58,9 +71,7 @@ class PolygonFirstMoment:
 
 class PolygonPolarSecondMoment:
     def setup(self):
-        try:
-            listOfPolygons[0].polar_second_moment_of_area()
-        except AttributeError:
+        if StrictVersion(sympy.__version__) < StrictVersion("1.5.0"):
             raise NotImplementedError
     def time_bench01(self):
         "Polygon.polar_second_moment_of_area for w in range(10)"
@@ -68,60 +79,16 @@ class PolygonPolarSecondMoment:
 
 class PolygonSectionModulus:
     def setup(self):
-        try:
-            listOfPolygons[0].section_modulus()
-        except AttributeError:
+        if StrictVersion(sympy.__version__) < StrictVersion("1.5.0"):
             raise NotImplementedError
     def time_bench01(self):
         "Polygon.section_modulus for w in range(10)"
         [listOfPolygons[w].section_modulus() for w in range(10)]
 
-class PolygonIsConvex:
-    def setup(self):
-        try:
-            listOfPolygons[0].is_convex()
-        except AttributeError:
-            raise NotImplementedError
-    def time_bench01(self):
-        "Polygon.is_convex() for w in range(10)"
-        [listOfPolygons[w].is_convex() for w in range(10)]
-
-class PolygonEnclosesPoint:
-    def setup(self):
-        try:
-            listOfPolygons[0].encloses_point(Point(randint(0,10),randint(0,10)))
-        except AttributeError:
-            raise NotImplementedError
-    def time_bench01(self,Point2=Point(randint(0,10),randint(0,10))):
-        "Polygon.encloses_point for w in range(10)"
-        [listOfPolygons[w].encloses_point(Point2) for w in range(10)]
-
-class PolygonArbitraryPoint:
-    def setup(self):
-        try:
-            listOfPolygons[0].arbitrary_point()
-        except AttributeError:
-            raise NotImplementedError
-    def time_bench01(self):
-        "Polygon.arbitrary_point for w in range(10)"
-        [listOfPolygons[w].arbitrary_point() for w in range(10)]
-
 class PolygonCutSection:
     def setup(self):
-        try:
-            cutListOfPolygons[0].cut_section(cutLines[0])
-        except AttributeError:
+        if StrictVersion(sympy.__version__) < StrictVersion("1.5.0"):
             raise NotImplementedError
     def time_bench01(self):
         "Polygon.cut_section for w in range(10)"
         [cutListOfPolygons[w].cut_section(cutLines[w]) for w in range(10)]
-
-class PolygonDistance:
-    def setup(self):
-        try:
-            listOfPolygons[0].distance(Point(randint(0,10),randint(0,10)))
-        except AttributeError:
-            raise NotImplementedError
-    def time_bench01(self):
-        "Polygon.distance() for w in range(10)"
-        [listOfPolygons[w].distance(Point(randint(0,10),randint(0,10))) for w in range(10)]
