0de02ba53312667f1fe7a44b1160a66a9f1b6d02
Sam Brockie <sgb39@cam.ac.uk>
2022-10-11 11:20:39 +0200

    Add docstrings to existing CSE benchmark

--- benchmarks/cse.py
+++ benchmarks/cse.py
@@ -1,9 +1,29 @@
+"""Benchmarks for SymPy's common subexpression elimination (CSE)."""
+
 from functools import reduce
 from operator import add
-import sympy as sp
+
+from sympy import cse, exp, sin, symbols, Matrix
+
 
 def _get_args_exprs(nexprs, nterms):
-    x = sp.symbols('x:%d' % nterms)
+    """Construct expressions with a number of symbols based on chained additions
+    and subtractions.
+
+    Examples
+    ========
+
+    >>> _get_args_exprs(1, 1)
+    ((x0,), [x0])
+    >>> _get_args_exprs(1, 3)
+    ((x0, x1, x2), [x0 - x1 + x2])
+    >>> _get_args_exprs(3, 1)
+    ((x0,), [x0, -x0, x0])
+    >>> _get_args_exprs(3, 3)
+    ((x0, x1, x2), [x0 - x1 + x2, -x0 + x1 - x2, x0 - x1 + x2])
+
+    """
+    x = symbols('x:%d' % nterms)
     exprs = [
         reduce(add, [x[j]*(-1)**(i+j) for j in range(nterms)]) for i in range(nexprs)
     ]
@@ -12,13 +32,10 @@ def _get_args_exprs(nexprs, nterms):
 
 class TimeCSE:
 
-    params = ((
-        (2, 8), (3, 8), (4, 8),
-        (2, 9), (2, 10), (2, 11)
-    ),)
+    params = (((2, 8), (3, 8), (4, 8), (2, 9), (2, 10), (2, 11), ), )
 
     def setup(self, nexprs_nterms):
         self.args, self.exprs = _get_args_exprs(*nexprs_nterms)
 
     def time_cse(self, nexprs_nterms):
-        sp.cse(self.exprs)
+        cse(self.exprs)
