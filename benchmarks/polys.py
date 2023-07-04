7a67669ca714617d6f9aac030a66e6b19b908dbe
Abhishek Patidar <1e9abhi1e10@gmail.com>
2023-07-04 14:49:22 +0530

    add benchmarks for linearly dense quartic inputs with quadratic GCDs

--- benchmarks/polys.py
+++ benchmarks/polys.py
@@ -1,5 +1,5 @@
-from sympy import symbols, prod
-from sympy.polys import ZZ, Poly
+from sympy import symbols
+from sympy.polys import ZZ, Poly, ring
 
 
 class TimePolyManyGens:
@@ -19,128 +19,22 @@ class TimePolyManyGens:
         self.px.is_linear
 
 
+class TimePolyprem:
+    """Benchmark for the prem method"""
 
-x, y, y1, y2, y3, y4, y5, y6, y7, y8, y9, y10 = symbols("x y y1 y2 y3 y4 y5 y6 y7 y8 y9 y10")
+    def setup(self):
+        self.R, self.x, self.y, self.y1, self.y2, self.y3, self.y4, self.y5, self.y6, self.y7, self.y8, self.y9, self.y10 = ring("x, y, y1, y2, y3, y4, y5, y6, y7, y8, y9, y10", ZZ)
+        self.y = [self.y1, self.y2, self.y3, self.y4, self.y5, self.y6, self.y7, self.y8, self.y9, self.y10]
+        self.values = {}
 
-y = [y1, y2, y3, y4, y5, y6, y7, y8, y9, y10] # set values of y for v = 1 to 10
+    def bench1_core_prem(self):
+        self.f = (self.x - self.y1 - 2)**2*(self.x + self.y1 + 1)**2
+        self.g = (self.x + self.y1 + 1)**2*(self.x + self.y1 + 2)**2
 
-R = ZZ[x, y, y1, y2, y3, y4, y5, y6, y7, y8, y9, y10]
+    def bench2_core_prem(self):
+        self.f = (self.x - self.y1 - self.y10 - self.y2 - self.y3 - self.y4 - self.y5 - self.y6 - self.y7 - self.y8 - self.y9 - 2)**2*(self.x + self.y1 + self.y10 + self.y2 + self.y3 + self.y4 + self.y5 + self.y6 + self.y7 + self.y8 + self.y9 + 1)**2
+        self.g = (self.x + self.y1 + self.y10 + self.y2 + self.y3 + self.y4 + self.y5 + self.y6 + self.y7 + self.y8 + self.y9 + 1)**2*(self.x + self.y1 + self.y10 + self.y2 + self.y3 + self.y4 + self.y5 + self.y6 + self.y7 + self.y8 + self.y9 + 2)**2
 
-# Case: Linearly dense quartic inputs with quadratic GCDs
-def bench1_core_prem():
-    for v in range(1, 11):
-        D = (1 + x + sum(y[:v])) ** 2
-        f = D * (-2 + x - sum(y[:v])) ** 2
-        g = D * (2 + x + sum(y[:v])) ** 2
-
-        return prem(f, g, x)
-
-def bench1_Poly_prem():
-    for v in range(1, 11):
-        D = (1 + x + sum(y[:v])) ** 2
-        f = D * (-2 + x - sum(y[:v])) ** 2
-        g = D * (2 + x + sum(y[:v])) ** 2
-
-        fp, gp = Poly(f, x, *y[:v]), Poly(g, x, *y[:v])
-
-        return fp.prem(gp)
-
-def bench1_PolyElement_prem():
-    for v in range(1, 11):
-        D = (1 + x + sum(y[:v])) ** 2
-        f = D * (-2 + x - sum(y[:v])) ** 2
-        g = D * (2 + x + sum(y[:v])) ** 2
-
-        fpe, gpe = R.from_sympy(f), R.from_sympy(g)
-
-        return fpe.prem(gpe)
-
-
-# Case: Sparse GCD and inputs where degree are proportional to the number of variables
-def bench2_core_prem():
-    for v in range(1, 11):
-        D = 1 + x ** (v + 1) + sum([y[i] ** (v + 1) for i in range(v)])
-        f = D * (-2 + x ** (v + 1) + sum([y[i] ** (v + 1) for i in range(v)]))
-        g = D * (2 + x ** (v + 1) + sum([y[i] ** (v + 1) for i in range(v)]))
-
-        return prem(f, g, x)
-
-def bench2_Poly_prem():
-    for v in range(1, 11):
-        D = 1 + x ** (v + 1) + sum([y[i] ** (v + 1) for i in range(v)])
-        f = D * (-2 + x ** (v + 1) + sum([y[i] ** (v + 1) for i in range(v)]))
-        g = D * (2 + x ** (v + 1) + sum([y[i] ** (v + 1) for i in range(v)]))
-
-        fp, gp = Poly(f, x, *y[:v]), Poly(g, x, *y[:v])
-
-        return fp.prem(gp)
-
-def bench2_PolyElement_prem():
-    for v in range(1, 11):
-        D = 1 + x ** (v + 1) + sum([y[i] ** (v + 1) for i in range(v)])
-        f = D * (-2 + x ** (v + 1) + sum([y[i] ** (v + 1) for i in range(v)]))
-        g = D * (2 + x ** (v + 1) + sum([y[i] ** (v + 1) for i in range(v)]))
-
-        fpe, gpe = R.from_sympy(f), R.from_sympy(g)
-
-        return fpe.prem(gpe)
-
-
-# Case: Quadratic non-monic GCD, F and G have other quadratic factors
-def bench3_core_prem():
-    for v in range(1, 11):
-        D = 1 + x**2 * y[0]**2 + sum([y[i]**2 for i in range(1, v)])
-        f = D * (-1 + x**2 - y[0]**2 + sum([y[i]**2 for i in range(1, v)]))
-        g = D * (2 + x * y[0] + sum(y[1:v]))**2
-
-        return prem(f, g, x)
-
-def bench3_Poly_prem():
-    for v in range(1, 11):
-        D = 1 + x**2 * y[0]**2 + sum([y[i]**2 for i in range(1, v)])
-        f = D * (-1 + x**2 - y[0]**2 + sum([y[i]**2 for i in range(1, v)]))
-        g = D * (2 + x * y[0] + sum(y[1:v]))**2
-
-        fp, gp = Poly(f, x, *y[:v]), Poly(g, x, *y[:v])
-
-        return fp.prem(gp)
-
-def bench3_PolyElement_prem():
-    for v in range(1, 11):
-        D = 1 + x**2 * y[0]**2 + sum([y[i]**2 for i in range(1, v)])
-        f = D * (-1 + x**2 - y[0]**2 + sum([y[i]**2 for i in range(1, v)]))
-        g = D * (2 + x * y[0] + sum(y[1:v]))**2
-
-        fpe, gpe = R.from_sympy(f), R.from_sympy(g)
-
-        return fpe.prem(gpe)
-
-
-# Case: Sparse non-monic quadratic inputs with linear GCDs
-def bench4_core_prem():
-    for v in range(1, 11):
-        D = -1 + x * (prod(y[:v]))
-        f = D * (3 + x * (prod(y[:v])))
-        g = D * (-3 + x * (prod(y[:v])))
-
-        return prem(f, g, x)
-
-def bench4_Poly_prem():
-    for v in range(1, 11):
-        D = -1 + x * (prod(y[:v]))
-        f = D * (3 + x * (prod(y[:v])))
-        g = D * (-3 + x * (prod(y[:v])))
-
-        fp, gp = Poly(f, x, *y[:v]), Poly(g, x, *y[:v])
-
-        return fp.prem(gp)
-
-def bench4_PolyElement_prem():
-    for v in range(1, 11):
-        D = -1 + x * (prod(y[:v]))
-        f = D * (3 + x * (prod(y[:v])))
-        g = D * (-3 + x * (prod(y[:v])))
-
-        fpe, gpe = R.from_sympy(f), R.from_sympy(g)
-
-        return fpe.prem(gpe)
+    def time_doit_prem(self):
+        self.values['bench1_core_prem'] =  self.prem(self.f, self.g)
+        self.values['bench2_core_prem'] =  self.prem(self.f, self.g)
