9fb370f866249a820c0b18d83d12772c353da10b
Abhishek Patidar <1e9abhi1e10@gmail.com>
2023-07-12 16:00:31 +0530

    add suggestions in teardown method

--- benchmarks/polys.py
+++ benchmarks/polys.py
@@ -20,10 +20,8 @@ class TimePolyManyGens:
 
 class _LinearDenseQuadraticGCD:
     def setup_polys(self, n):
-        self.x, *self.y = symbols("x, y1:9")
-        self.R1 = [self.x] + list(self.y)
-        self.R = ZZ[self.R1]
-        x, y, R = self.x, self.y, self.R
+        x, *y = symbols("x, y1:{}".format(n+1))
+        R = ZZ[[x] + list(y)]
         D = (1 + x + sum(y[:n])) ** 2
         f = D * (-2 + x - sum(y[:n])) ** 2
         g = D * (2 + x + sum(y[:n])) ** 2
@@ -34,10 +32,8 @@ class _LinearDenseQuadraticGCD:
 
 class _SparseGCDHighDegree:
     def setup_polys(self, n):
-        self.x, *self.y = symbols("x, y1:9")
-        self.R1 = [self.x] + list(self.y)
-        self.R = ZZ[self.R1]
-        x, y, R = self.x, self.y, self.R
+        x, *y = symbols("x, y1:{}".format(n+1))
+        R = ZZ[[x] + list(y)]
         D = 1 + x ** (n + 1) + sum([y[i] ** (n + 1) for i in range(n)])
         f = D * (-2 + x ** (n + 1) + sum([y[i] ** (n + 1) for i in range(n)]))
         g = D * (2 + x ** (n + 1) + sum([y[i] ** (n + 1) for i in range(n)]))
@@ -48,10 +44,8 @@ class _SparseGCDHighDegree:
 
 class _QuadraticNonMonicGCD:
     def setup_polys(self, n):
-        self.x, *self.y = symbols("x, y1:9")
-        self.R1 = [self.x] + list(self.y)
-        self.R = ZZ[self.R1]
-        x, y, R = self.x, self.y, self.R
+        x, *y = symbols("x, y1:{}".format(n+1))
+        R = ZZ[[x] + list(y)]
         D = 1 + x ** 2 * y[0] ** 2 + sum([y[i] ** 2 for i in range(1, n)])
         f = D * (-1 + x ** 2 - y[0] ** 2 + sum([y[i] ** 2 for i in range(1, n)]))
         g = D * (2 + x * y[0] + sum(y[1:n])) ** 2
@@ -62,10 +56,8 @@ class _QuadraticNonMonicGCD:
 
 class _SparseNonMonicQuadratic:
     def setup_polys(self, n):
-        self.x, *self.y = symbols("x, y1:9")
-        self.R1 = [self.x] + list(self.y)
-        self.R = ZZ[self.R1]
-        x, y, R = self.x, self.y, self.R
+        x, *y = symbols("x, y1:{}".format(n+1))
+        R = ZZ[[x] + list(y)]
         D = -1 + x * prod(y[:n])
         f = D * (3 + x * prod(y[:n]))
         g = D * (-3 + x * prod(y[:n]))
@@ -75,8 +67,6 @@ class _SparseNonMonicQuadratic:
 
 
 class _TimePREM:
-    """Benchmarks for `prem` method in polynomials."""
-
     def setup(self, n, method):
         (f, g, fp, gp, fpe, gpe, x, y, R) = self.setup_polys(n)
 
@@ -84,28 +74,29 @@ class _TimePREM:
 
         prem_f_g_x = rem(f * LC(g, x) ** (degree(f, x) - degree(g, x) + 1), g, x)
 
-        self.ref_expr = prem_f_g_x
-        self.ref_ring = R(prem_f_g_x.as_expr())
-
         if method == 'prem':
             self.func = lambda: prem(f, g, x)
+            self.expected = prem_f_g_x
 
         elif method == 'poly':
             self.func = lambda: fp.prem(gp)
+            self.expected = prem_f_g_x
 
         elif method == 'sparse':
             self.func = lambda: fpe.prem(gpe)
+            self.expected = R(prem_f_g_x.as_expr())
 
     def teardown(self, n, method):
-        for key, val in self.values.items():
-            if key == 'sparse':
-                if (self.ref_ring - val) != 0:
-                    raise ValueError("Incorrect result, invalid timing:"
-                                        " %s != %s" % (self.ref_ring, val))
+        self.result = self.values[method]  # Get the result for the current method
 
-            elif (self.ref_expr - val).simplify() != 0:
-                raise ValueError("Incorrect result, invalid timing:"
-                                    " %s != %s" % (self.ref_expr, val))
+        if method == 'prem':
+            assert self.result == self.expected
+
+        elif method == 'poly':
+            assert self.result == self.expected
+
+        elif method == 'sparse':
+            assert self.result == self.expected
 
     def time_prem_methods(self, n, method):
         self.values[str(method)] = self.func()
