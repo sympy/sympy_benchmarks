efac051b9f9e610464551f2f3a7a1aef6d05d7bf
Abhishek Patidar <1e9abhi1e10@gmail.com>
2023-07-14 02:50:12 +0530

    Add suggestions

--- benchmarks/polys.py
+++ benchmarks/polys.py
@@ -1,5 +1,6 @@
 from sympy import symbols, prod, prem, rem, degree, LC
-from sympy.polys import ZZ, Poly
+from sympy.polys import QQ, Poly
+
 
 class TimePolyManyGens:
     """Time using a Poly with many generators"""
@@ -18,103 +19,148 @@ class TimePolyManyGens:
         self.px.is_linear
 
 
-class _LinearDenseQuadraticGCD:
-    def setup_polys(self, n):
-        x, *y = symbols("x, y1:{}".format(n+1))
-        R = ZZ[[x] + list(y)]
-        D = (1 + x + sum(y[:n])) ** 2
-        f = D * (-2 + x - sum(y[:n])) ** 2
-        g = D * (2 + x + sum(y[:n])) ** 2
-        fp, gp = Poly(f, x, *y[:n]), Poly(g, x, *y[:n])
-        fpe, gpe = R.from_sympy(f), R.from_sympy(g)
-        return f, g, fp, gp, fpe, gpe, x, y, R
+class _GCDExample:
+    """A benchmark example with two polynomials and their gcd."""
+
+    def __init__(self, n):
+        f, g, d, syms = self.make_poly(n)
+        self.f = f
+        self.g = g
+        self.d = d
+        self.x = syms[0]
+        self.y = syms[1:]
+        self.syms = syms
+        self.ring = QQ[syms]
+
+    def to_expr(self, expr):
+        return expr
+
+    def to_poly(self, expr):
+        return Poly(expr, self.syms)
+
+    def to_ring(self, expr):
+        return self.ring(expr)
+
+    def as_expr(self):
+        return (self.f, self.g, self.d, self.syms)
+
+    def as_poly(self):
+        return (self.to_poly(self.f), self.to_poly(self.g), self.to_poly(self.d))
+
+    def as_ring(self):
+        return (self.to_ring(self.f), self.to_ring(self.g), self.to_ring(self.d), self.ring)
 
 
-class _SparseGCDHighDegree:
-    def setup_polys(self, n):
-        x, *y = symbols("x, y1:{}".format(n+1))
-        R = ZZ[[x] + list(y)]
-        D = 1 + x ** (n + 1) + sum([y[i] ** (n + 1) for i in range(n)])
-        f = D * (-2 + x ** (n + 1) + sum([y[i] ** (n + 1) for i in range(n)]))
-        g = D * (2 + x ** (n + 1) + sum([y[i] ** (n + 1) for i in range(n)]))
-        fp, gp = Poly(f, x, *y[:n]), Poly(g, x, *y[:n])
-        fpe, gpe = R.from_sympy(f), R.from_sympy(g)
-        return f, g, fp, gp, fpe, gpe, x, y, R
+class _LinearDenseQuadraticGCD(_GCDExample):
+    """A pair of linearly dense quartic inputs with quadratic GCDs"""
+
+    def make_poly(self, n):
+        x, *y = syms = symbols("x, y1:{}".format(n+1))
+        d = (1 + x + sum(y[:n])) ** 2
+        f = d * (-2 + x - sum(y[:n])) ** 2
+        g = d * (2 + x + sum(y[:n])) ** 2
+        return f, g, d, syms
+
+class _SparseGCDHighDegree(_GCDExample):
+    """A pair of polynomials in n symbols with a high degree sparse GCD."""
+
+    def make_poly(self, n):
+        x, *y = syms = symbols("x, y1:{}".format(n+1))
+        d = 1 + x ** (n + 1) + sum([y[i] ** (n + 1) for i in range(n)])
+        f = d * (-2 + x ** (n + 1) + sum([y[i] ** (n + 1) for i in range(n)]))
+        g = d * (2 + x ** (n + 1) + sum([y[i] ** (n + 1) for i in range(n)]))
+        return f, g, d, syms
 
 
-class _QuadraticNonMonicGCD:
-    def setup_polys(self, n):
-        x, *y = symbols("x, y1:{}".format(n+1))
-        R = ZZ[[x] + list(y)]
-        D = 1 + x ** 2 * y[0] ** 2 + sum([y[i] ** 2 for i in range(1, n)])
-        f = D * (-1 + x ** 2 - y[0] ** 2 + sum([y[i] ** 2 for i in range(1, n)]))
-        g = D * (2 + x * y[0] + sum(y[1:n])) ** 2
-        fp, gp = Poly(f, x, *y[:n]), Poly(g, x, *y[:n])
-        fpe, gpe = R.from_sympy(f), R.from_sympy(g)
-        return f, g, fp, gp, fpe, gpe, x, y, R
+class _QuadraticNonMonicGCD(_GCDExample):
+    """A pair of quadratic polynomials with a non-monic GCD."""
+
+    def make_poly(self, n):
+        x, *y = syms = symbols("x, y1:{}".format(n+1))
+        d = 1 + x ** 2 * y[0] ** 2 + sum([y[i] ** 2 for i in range(1, n)])
+        f = d * (-1 + x ** 2 - y[0] ** 2 + sum([y[i] ** 2 for i in range(1, n)]))
+        g = d * (2 + x * y[0] + sum(y[1:n])) ** 2
+        return f, g, d, syms
 
 
-class _SparseNonMonicQuadratic:
-    def setup_polys(self, n):
-        x, *y = symbols("x, y1:{}".format(n+1))
-        R = ZZ[[x] + list(y)]
-        D = -1 + x * prod(y[:n])
-        f = D * (3 + x * prod(y[:n]))
-        g = D * (-3 + x * prod(y[:n]))
-        fp, gp = Poly(f, x, *y[:n]), Poly(g, x, *y[:n])
-        fpe, gpe = R.from_sympy(f), R.from_sympy(g)
-        return f, g, fp, gp, fpe, gpe, x, y, R
+class _SparseNonMonicQuadratic(_GCDExample):
+    """A pair of sparse non-monic quadratic polynomials with linear GCDs."""
+
+    def make_poly(self, n):
+        x, *y = syms = symbols("x, y1:{}".format(n+1))
+        d = -1 + x * prod(y[:n])
+        f = d * (3 + x * prod(y[:n]))
+        g = d * (-3 + x * prod(y[:n]))
+        return f, g, d, syms
+
+class _TimeOP:
+    """
+    Benchmarks comparing Poly implementations of a given operation.
+    """
+    param_names = [
+        'size',
+        'impl',
+    ]
+
+    def setup(self, n, impl):
+
+        examples = self.GCDExampleCLS(n)
+
+        expected = self.expected(*examples.as_expr())
+
+        if impl == 'expr':
+            func = self.get_func_expr(*examples.as_expr())
+            expected = examples.to_expr(expected)
+        elif impl == 'dense':
+            func = self.get_func_poly(*examples.as_poly())
+            expected = examples.to_poly(expected)
+        elif impl == 'sparse':
+            func = self.get_func_sparse(*examples.as_ring())
+            expected = examples.to_ring(expected)
+
+        self.func = func
+        self.expected_result = expected
+
+    def time_op(self, n, impl):
+        self.returned_result = self.func()
+
+    def teardown(self, n, impl):
+        assert self.expected_result == self.returned_result
 
 
-class _TimePREM:
-    def setup(self, n, method):
-        (f, g, fp, gp, fpe, gpe, x, y, R) = self.setup_polys(n)
+class _TimePREM(_TimeOP):
 
-        self.values = {}
-
+    def expected(self, f, g, d, syms):
+        x = syms[0]
         prem_f_g_x = rem(f * LC(g, x) ** (degree(f, x) - degree(g, x) + 1), g, x)
+        return prem_f_g_x
 
-        if method == 'prem':
-            self.func = lambda: prem(f, g, x)
-            self.expected = prem_f_g_x
+    def get_func_expr(self, f, g, d, syms):
+        x = syms[0]
+        return lambda: prem(f, g, x)
 
-        elif method == 'poly':
-            self.func = lambda: fp.prem(gp)
-            self.expected = prem_f_g_x
+    def get_func_poly(self, f, g, d):
+        return lambda: f.prem(g)
 
-        elif method == 'sparse':
-            self.func = lambda: fpe.prem(gpe)
-            self.expected = R(prem_f_g_x.as_expr())
-
-    def teardown(self, n, method):
-        self.result = self.values[method]  # Get the result for the current method
-        assert self.result == self.expected
-
-    def time_prem_methods(self, n, method):
-        self.values[str(method)] = self.func()
-
-    param_names = ['degree', 'Implementation_methods']
+    def get_func_sparse(self, f, g, d, ring):
+        return lambda: f.prem(g)
 
 
-class TimePREMLinearDenseQuadraticGCD(_LinearDenseQuadraticGCD, _TimePREM):
-    """Calculate time for Linearly dense quartic inputs with quadratic GCDs polynomials."""
-
-    params = [(1, 3 , 5), ('prem', 'poly', 'sparse')] # This case is slow for n=8.
-
-
-class TimePREMQuadraticNonMonicGCD(_QuadraticNonMonicGCD, _TimePREM):
-    """Calculate time for Quadratic non-monic GCD, F and G have other quadratic factors polynomials."""
-
-    params = [(1, 3 , 5), ('prem', 'poly', 'sparse')] # This case is slow for n=8.
+class TimePREM_LinearDenseQuadraticGCD(_TimePREM):
+    GCDExampleCLS = _LinearDenseQuadraticGCD
+    params = [(1, 3, 5), ('expr', 'dense', 'sparse')] # This case is slow for n=8.
 
 
-class TimePREMSparseGCDHighDegree(_SparseGCDHighDegree, _TimePREM):
-    """Calculate Sparse GCD and inputs where degree is proportional to the number of variables polynomials."""
+class TimePREM_SparseGCDHighDegree(_TimePREM):
+    GCDExampleCLS = _SparseGCDHighDegree
+    params = [(1, 3, 5, 8), ('expr', 'dense', 'sparse')]
 
-    params = [(1, 3 , 5, 8), ('prem', 'poly', 'sparse')]
+
+class TimePREM_QuadraticNonMonicGCD(_TimePREM):
+    GCDExampleCLS = _QuadraticNonMonicGCD
+    params = [(1, 3, 5), ('expr', 'dense', 'sparse')] # This case is slow for n=8.
 
 
-class TimePREMSparseNonMonicQuadratic(_SparseNonMonicQuadratic, _TimePREM):
-    """Calculate time for Sparse non-monic quadratic inputs with linear GCDs polynomials."""
-
-    params = [(1, 3 , 5, 8), ('prem', 'poly', 'sparse')]
+class TimePREM_SparseNonMonicQuadratic(_TimePREM):
+    GCDExampleCLS = _SparseNonMonicQuadratic
+    params = [(1, 3, 5, 8), ('expr', 'dense', 'sparse')]
