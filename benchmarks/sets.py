54c36f2dea252a1dd3c003f3c3ece783608605f2
rajeshkotaru <kotarurajesh2004@gmail.com>
2024-02-15 02:01:21 +0530

    Update sets.py

--- benchmarks/sets.py
+++ benchmarks/sets.py
@@ -1,5 +1,5 @@
 import sympy as sp
-from sympy import Interval,S,Reals
+from sympy import Interval,S,Reals,Union
 """
 list of benchmarking set properties:
 1)set.boundary
@@ -8,6 +8,8 @@ list of benchmarking set properties:
 4)set.interior
 5)set.is_closed
 6)set.is_open
+7)set.kind
+8)set.measure
 """
 class TimeSetProperties:
     def setup(self):
@@ -23,14 +25,22 @@ class TimeSetProperties:
     def time_is_closed(self):
         Interval(0,1).is_closed  
     def time_is_open(self):
-        S.Reals.is_open            
+        S.Reals.is_open   
+    def time_kind(self):
+        Interval(0,1).kind 
+    def time_measure(self):
+        Union(Interval(-1,2),Interval(4,5)).measure               
 """
 list of benchmarking set functions:
 1)set.complement()
 2)set.contains()
-3)set.intersect()/set.intersection()-> as intersection() is alias for intersect().
-4)set.is_disjoint()
-5)
+3)set.intersect()/set.intersection()-> as intersection() is alias  intersect().
+4)set.is_disjoint()/set.isdisjoint()->as isdisjoint() is alias is_disjoint().
+5)set.is_proper_subset()
+6)set.is_proper_superset()
+7)set.is_subset()/set.issubset()->as issubset() is alias is_subset().
+8)set.is_superset()/set.issuperset()->as issuperset() is alias is_superset().
+8)set.powerset()
 """
 class TimeSetFunctions:
     def  time_complement(self):
@@ -41,5 +51,19 @@ class TimeSetFunctions:
         Interval(0,1).intersect(Interval(1,2))   
     def time_is_disjoint(self):
         Interval(0, 2).is_disjoint(Interval(3, 4)) 
+    def time_is_proper_subset(self):
+        Interval(0, 1).is_proper_subset(Interval(0, 0.5))
+    def time_is_proper_superset(self):
+        Interval(0,1).is_proper_superset(Interval(-1,2))
+    def time_is_subset(self):
+        Interval(0,1).is_subset(Interval(0,1))
+    def time_is_superset(self):
+        Interval(0,1).is_superset(Interval(-2,4))
+    def time_powerset(self):
+        Interval(0,1).powerset()    
+
+                                     
+
+
 
 
