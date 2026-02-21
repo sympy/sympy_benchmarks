5a6d99a45838d84d34b62fcd1279ec1f1fa7e97f
SirMarcoss <marco.mazzone06@gmail.com>
2026-02-21 16:48:19 +0100

    Add benchmarks for printing module using various math structures

---
+++ benchmarks/printing.py
@@ -0,0 +1,51 @@
+from sympy import Symbol, Matrix, latex, log
+
+
+class TimeMatrixPrinting:
+    """Benchmark for grid structures"""
+    params = [10, 20, 30]
+
+    def setup(self, n):
+        x = Symbol('x')
+        self.expr = Matrix(n, n, lambda i, j: x ** (i + j))
+
+    def time_str(self, n):
+        str(self.expr)
+
+    def time_latex(self, n):
+        latex(self.expr)
+
+
+class TimePolyPrintin:
+    """Benchmark for polynomial structure"""
+    params = [10, 50, 500]
+
+    def setup(self, n):
+        x = Symbol('x')
+        y = Symbol('y')
+        self.expr = ((x + y) ** n).expand()
+
+    def time_str(self, n):
+        str(self.expr)
+
+    def time_latex(self, n):
+        latex(self.expr)
+
+
+class TimeNestedExprPrinting:
+    """Benchmark for deep nested trees structure"""
+    params = [10, 50, 100]
+
+    def setup(self, n):
+        x = Symbol('x')
+        expr = x
+        for _ in range(n):
+            expr = log(expr)
+
+        self.expr = expr
+
+    def time_str(self, n):
+        str(self.expr)
+
+    def time_latex(self, n):
+        latex(self.expr)
